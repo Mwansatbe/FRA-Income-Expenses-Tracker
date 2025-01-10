6c0741f3132c5318e78ba9ec969d7364e367332e
Mwansa Chilekwa <mwansachille@gmail.com>
2025-01-10 16:20:05 +0200

    Add custom styles to site Admin

--- income/admin.py
+++ income/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 from . models import Income, Source
 # Register your models here.
 
-admin.site.register(Income)
+
+
+
+
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ("amount", "description", "owner", "source", "date")
+    search_fields = ("description", "source", "date")
+    
+    # Add filters to the sidebar for numeric and date fields
+    list_filter = ("source", "date", "amount")
+    
+    list_per_page=5
+
+admin.site.register(Income, IncomeAdmin)
 admin.site.register(Source)
+
+
+
+
+
+
+# class IncomeAdmin(admin.ModelAdmin):
+#   list_display=("amount", "description", "owner", "source", "date")
+#   search_fields=("description", "source", "date")
+# admin.site.register(Income, IncomeAdmin)
+# admin.site.register(Source)
