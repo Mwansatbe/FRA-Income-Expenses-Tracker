03a9567654c17b26791b1b612b8cefa7e2d499e4
Mwansa Chilekwa <mwansachille@gmail.com>
2025-01-15 08:36:26 +0200

    add an expense chart report

--- expenses/urls.py
+++ expenses/urls.py
@@ -7,6 +7,8 @@ urlpatterns =[
   path('add-expense',views.add_expense, name='add-expense'),
   path('edit-expense/<int:id>',views.expense_edit, name='edit-expense'),
   path('delete-expense/<int:id>', views.delete_expense, name='delete-expense'),
-  path('search-expense', csrf_exempt(views.search_expense), name='search_expense')
+  path('search-expense', csrf_exempt(views.search_expense), name='search_expense'),
+  path("expense_category_summary", views.expense_category_summary, name="expense_category_summary"),
+  path("stats_view", views.stats_view, name ="stats_view")
 
 ]
